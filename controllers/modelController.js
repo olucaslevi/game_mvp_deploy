5e8432b89a214104ee390ce8b3e74ca79de03657
Levi <arrylucas@gmail.com>
2024-09-05 20:05:42 -0700

    fix urls commits

--- controllers/modelController.js
+++ controllers/modelController.js
@@ -71,13 +71,13 @@ class ModelController {
         }
     }
     createSoldier(position, color, callback) {
-        const url = 'https://olucaslevi.github.io/game_mvp_deploy/models/lagarto-with-AI.glb'
+        const url = './../game_mvp_deploy/models/lagarto-with-AI.glb';
         const rotation = new THREE.Vector3(0, 0, 0);
         const scale = 0.06;
         this.loadModel(url, position, rotation, scale, color, callback);
     }
     createTower(position, color, callback) {
-        const url = 'https://olucaslevi.github.io/game_mvp_deploy/models/towers.glb'
+        const url = './../game_mvp_deploy/models/tower.glb';
         const rotation = new THREE.Vector3(1, 0, 0);
         const scale = 1;
         this.loadModel(url, position, rotation, scale, color, callback);
@@ -89,7 +89,7 @@ class ModelController {
         this.loadModel(url, position, rotation, scale, color, callback);
     }
     createTerrain(callback) {
-        const url = '/game_mvp_deploy/models/terrain.glb';
+        const url = './../game_mvp_deploy/models/terrain.glb';
         const position = new THREE.Vector3(0, 0, 0);
         const rotation = new THREE.Vector3(0, 0, 0);
         const scale = 2;
@@ -102,13 +102,13 @@ class ModelController {
     //     this.loadModel(url, position, rotation, scale, color, callback);
     // }
     createWarrior(position, callback) {
-        const url = 'https://olucaslevi.github.io/game_mvp_deploy/models/warrior-with-AI.glb';
+        const url = './../game_mvp_deploy/models/warrior-with-AI.glb';
         const rotation = new THREE.Vector3(0, 0, 0);
         const scale = 0.06;
         this.loadModel(url, position, rotation, scale, null, callback);
     }
     createArcher(position, callback) {
-        const url = 'https://olucaslevi.github.io/game_mvp_deploy/models/archer-with-AI.glb';
+        const url = './../game_mvp_deploy/models/archer-with-AI.glb';
         const rotation = new THREE.Vector3(0, 0, 0);
         const scale = 0.06;
         this.loadModel(url, position, rotation, scale, null, callback);
